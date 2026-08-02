13e16c5c1d01cbeabcfe1774a6115cb5395b43cd
Prompt2Pro <your-github-team.kynovix@gmail.com>
2026-08-02 15:10:43 +0100

    Day 1: Add Personal Profile Generator and Age Calculator

---
+++ Project1_age.py
@@ -0,0 +1,5 @@
+name = input ("Enter your name:  ")
+birth_year = int(input("Enter your birth year: "))
+current_year = int(input("Enter your current year: "))
+age = current_year - birth_year
+print (f"my name is {name} and age is {age}.")
---
+++ Project_1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:  ")
+city = input("What is your city: ")
+age = int(input("Enter your age: "))
+profession = input("Enter your profession: ")
+hobbies = input("Enter your hobbies: ")
+gender = input("Enter your gender: ")
+nationality = input("Enter your nationality: ")
+
+print(f"My name is {name} and I am from {city}, and {age} years old, my profession is {profession}, and my hobbies are {hobbies}, gender is {gender}, nationality is {nationality}. ")
